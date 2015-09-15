def8904f683f8876b6af325e6d16c370721844dc
Richard Remer <rich@zingle.me>
2015-09-14 18:36:05 -0700

    abstrac storage

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var LaterServer = require("./lib/later-server");
+var LaterServer = require("./lib/later-server"),
+    LaterStorage = require("./lib/later-storage");
 
 module.exports = {
     LaterServer: LaterServer,
+    LaterStorage: LaterStorage,
     createServer: LaterServer.create
 };

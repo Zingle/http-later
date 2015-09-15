3bfb14e7a87f5be28c7207d65b2f5424d5b98515
Richard Remer <rich@zingle.me>
2015-09-15 13:00:43 -0700

    improve listen options and logging

--- README.md
+++ README.md
@@ -4,20 +4,40 @@ Queue incoming HTTP requests and replay them later.
 
 Usage
 -----
-```sh
-Usage: http-later [[-m|--method=<method>], ...] [[-h||--host=<host>], ...]
-    [-p|--port=<port>] [[-P|--path=<path>], ...] [-r|--replay]
-    [[-v|--verbose], ...] [-q|--quiet|-s|--silent]
+```
+Usage: http-later [[-v|--verbose], ...] [-q|--quiet|-s|--silent]
+    [[-A|--accept=<accepted>]] [-r|--replay] [-T|--tls-dir=<tls-dir>]
+
+  -A  --accept=<accepted>   accept requests; see accept options below
+  -q  --quiet               disable all output, except errors
+  -s  --silent              disable all output, even errors
+  -T  --tls-dir=<tld-dir>   path prefix for accepted 'tls' option values
+  -v  --verbose             increase amount of output; can be used multiple
+                            times
+
+accept options
+  host      host name to listen on
+  port      listen port; TLS defaults to 443, otherwise defaults to 80
+  method    HTTP method to allowed
+  methods   colon-delimited HTTP methods allowed
+  path      path prefix; 404 for paths which do not begin with prefix
+  paths     colon-delimited path prefixes accepted
+  tls       paths to TLS certs: [<pfx>|<cert>:<key>[:<ca>]]
+
+  Options are comma-delimited, name and value are colon-delimited.
 ```
 
 Examples
 --------
-Accept incoming requests on port 80, queue the request, and return 202 Accepted.
 ```sh
-http-later -p80 -vv
+CERT_OPTS=tls:zingle.crt:zingle.key:zingle-ca.crt
+http-later -vv \
+    --tls-dir=/etc/ssl/private \
+    --listen=host:dev-admin.zingle.me,path:/res,$CERT_OPTS \
+    --listen=host:dev-dashboard.zingle.me:path=/service:$CERT_OPTS \
 ```
 
 Replay queued requests.
 ```sh
-http-later --replay
+http-later -vv --replay
 ```
--- bin/http-later.js
+++ bin/http-later.js
@@ -1,5 +1,4 @@
-var fs = require("fs"),
-    squabble = require("squabble").createParser(),
+var squabble = require("squabble").createParser(),
     later = require(".."),
     noop = require("../lib/noop"),
     server, port, hosts,
@@ -10,23 +9,20 @@ var fs = require("fs"),
 require("colors");
 
 // patch console object
+console.error = noop(console.error).enable();
 console.log = noop(console.log).enable();
 console.info = noop(console.info).disable();
-console.warn = noop(console.warn).disable();
+console.dir = noop(console.dir).disable();
+console.trace = noop(console.trace).disable();
 
 // set up CLI argument parsing
 squabble.shortOpts().longOpts().stopper()
-    .list("-m", "--method")
-    .list("-h", "--host")
-    .list("-P", "--path")
+    .list("-A", "--accept")
     .count("-v", "--verbose")
     .flag("-q", "--quiet")
     .flag("-s", "--silent")
     .flag("-r", "--replay")
-    .option("-p", "--port")
-    .option("--tls-cert")
-    .option("--tls-key")
-    .option("--tls-ca");
+    .option("-T", "--tls-dir");
 
 // parse global CLI args
 args = squabble.parse();
@@ -34,57 +30,115 @@ args = squabble.parse();
 // configure logging
 if (args.named["--quiet"] || args.named["--silent"]) {
     console.log.disable();
+    if (args.named["--silent"]) console.error.disable();
 } else {
-    if (args.named["--verbose"] > 0) console.warn.enable();
-    if (args.named["--verbose"] > 1) console.info.enable();
+    if (args.named["--verbose"] > 0) console.info.enable();
+    if (args.named["--verbose"] > 1) console.dir.enable();
+    if (args.named["--verbose"] > 2) console.trace.enable();
 }
 
-// read CLI args and build server options object
-args = squabble.parse();
-if (args.named["--method"].length > 0) opts.methods = args.named["--method"];
-if (args.named["--host"].length > 0) opts.hosts = args.named["--host"];
-if (args.named["--path"].length > 0) opts.paths = args.named["--path"];
-if (args.named["--port"]) opts.port = args.named["--port"];
+// set options
+if (args.named["--tls-dir"]) opts.tlsDir = args.named["--tls-dir"];
 
-// validate and apply TLS options
-if (args.named["--tls-cert"]) {
-    if (!args.named["--tls-key"]) {
-        console.error("TLS cert requires key (try --tls-key");
-        process.exit(1);
-    }
-
-    opts.tls = {
-        cert: fs.readFileSync(args.named["--tls-cert"]),
-        key: fs.readFileSync(args.named["--tls-key"])
-    };
-
-    if (args.named["--tls-ca"]) {
-        opts.tls.ca = fs.readFileSync(args.named["--tls-ca"]);
-    }
-}
-
-// create server and log start message
+// create server
 server = later.createServer(opts);
 console.log("starting server".green);
 
-// shutdown on server error after logging
+// on server error, write to console and shutdown
 server.on("error", function(err) {
     console.error(String(err).red);
-    console.info("shutting down".green);
+    console.log("shutting down".green);
     server.close(function() {
         process.exit(1);
     });
 });
 
+// log info about listeners
+server.on("listening", function(httpServer) {
+    var scheme = httpServer.laterOpts.tls ? "https" : "http",
+        host = httpServer.laterOpts.host || "*",
+        path = httpServer.laterOpts.path || "/",
+        port = parseInt(httpServer.laterOpts.port),
+        url;
+
+    if (port === 443 && scheme === "https") port = "";
+    else if (port === 80 && scheme === "http") port = "";
+    else if (port) port = ":" + port;
+    else port = "";
+
+    url = scheme + "://" + host + port + path;
+    console.log(("listening on " + url).green);
+});
+
+// log some other server events
+server.on("replay", function() {console.log("replaying".gray);});
+server.on("drain", function() {console.log("drained".gray);});
+
+// log responses to incoming requests
+server.on("request", function(res, req) {
+    var status = res.statusCode;
+
+    if (status < 200) status = String(status).yellow;
+    else if (status < 300) status = String(status).cyan;
+    else if (status < 500) status = String(status).yellow;
+    else status = String(status).red;
+
+    console.log(status + " " + req.method + " " + req.url);
+});
+
+// log responses to replayed requests
+server.on("response", function(res, req) {
+    var status = String(res.statusCode).magenta;
+    console.log(status + " " + req.method + " " + req.url);
+});
+
 // begin replay of queued requests
 if (args.named["--replay"]) server.replay();
 
-// begin listening on server
-port = opts.port || process.env.LATER_PORT || 2112;
-hosts = opts.hosts ? opts.hosts : [undefined];
-hosts.forEach(function(host) {
-    server.listen(port, host, function(address) {
-        console.log(("listening on " + address.address + ":" + address.port).blue);
-    });
+// begin accepting and queueing requests
+args.named["--accept"].forEach(function(accept) {
+    var opts = readOpts(accept),
+        tls;
+
+    // parse tls option if present
+    if (opts.tls) {
+        tls = opts.tls.split(":");
+        if (tls.length === 1) {
+            opts.tls = {pfx: tls.shift()};
+        } else if (tls.length === 2) {
+            opts.tls = {cert: tls.shift(), key: tls.shift()};
+        } else if (tls.length === 3) {
+            opts.tls = {cert: tls.shift(), key: tls.shift(), ca: tls.shift()};
+        } else {
+            console.error(String("unrecognized 'tls' option " + opts.tls).red);
+            process.exit(1);
+        }
+    }
+
+    // start accepting
+    server.accept(opts);
 });
+
+/**
+ * Parse an option string such as the one used with --accept.
+ * @param {string} opts
+ * @returns {object}
+ */
+function readOpts(opts) {
+    var result = {};
+
+    opts.split(",").forEach(function(opt) {
+        var parts = opt.split(":"),
+            msg;
+
+        if (parts.length < 2 || !parts[0] || parts[0] in result) {
+            msg = String("invalid or unrecognized option '" + opt + "'").red;
+            console.log(msg);
+        } else {
+            result[parts[0]] = parts.slice(1).join(":");
+        }
+    });
+    
+    return result;
+}
 
